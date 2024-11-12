4250f2492872c3cf531449c9fa11dd6ec6cb2c39
Branislav <bbogosavac1999@gmail.com>
2024-11-11 23:06:01 -0700

    done6

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ app.get('/', (req, res) => {
 });
 
 app.use(router);
+app.use(async (req, res, next) => {
+    next({status: 404, message: 'Sorry, the page cannot be found'});
+});
+
+app.use((err, req, res, send) => {
+    if (err.status === 404) {
+        res.status(404).send(err.message);
+    } else {
+        next(err);
+    }
+});
 
 app.listen(5000, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
