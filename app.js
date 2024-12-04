aea0e18ffbd16f4cb9031b8cf5e542962277b9d2
Branislav Bogosavac <97567320+branislav999@users.noreply.github.com>
2024-12-03 23:05:26 -0700

    corrected

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://cse341-project2-a80q.onrender.com/auth/google/callback/auth/google/callback"
+    callbackURL: "https://cse341-project2-a80q.onrender.com/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
